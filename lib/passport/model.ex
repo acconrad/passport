f996ac7ab623ea1119b79733f3951e8e39277e53
Adam Conrad <acconrad@gmail.com>
2017-04-21 00:35:45 -0400

    Update model.ex

--- lib/passport/model.ex
+++ lib/passport/model.ex
@@ -1,7 +1,4 @@
 defmodule Passport.Model do
-  import Ecto.Query
-  import String, only: [downcase: 1]
-
   def repo do
     Application.get_env(:passport, :repo)
   end
@@ -12,10 +9,10 @@ defmodule Passport.Model do
 
   def find_user_by_email(email) do
     email = String.downcase(email)
-    user_model |> repo.get_by(email: email)
+    user_model() |> repo().get_by(email: email)
   end
 
   def find_user_by_id(id) do
-    user_model |> repo.get(id)
+    user_model() |> repo().get(id)
   end
 end
