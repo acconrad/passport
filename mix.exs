772ccbf7a381dd1ddeae3e340082d168c4e64fb4
Adam Conrad <acconrad@gmail.com>
2017-04-20 23:51:52 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -3,42 +3,34 @@ defmodule Passport.Mixfile do
 
   def project do
     [app: :passport,
-     description: "Provides authentication for phoenix applications",
-     version: "0.5.3",
-     elixir: "~> 1.3.4",
-     package: package,
-     deps: deps,
-     test_coverage: [tool: ExCoveralls]]
+     version: "0.5.4",
+     elixir: "~> 1.4",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
   end
 
   # Configuration for the OTP application
   #
-  # Type `mix help compile.app` for more information
+  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :comeonin]]
-  end
-
-  defp package do
-    [contributors: ["Shankar Dhanasekaran - (shankardevy)"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/opendrops/passport"}]
+    # Specify extra applications you'll use from Erlang/Elixir
+    [extra_applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
   #
-  #   {:mydep, "~> 0.3.0"}
+  #   {:my_dep, "~> 0.3.0"}
   #
   # Or git/path repositories:
   #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
   #
-  # Type `mix help deps` for more examples and options
+  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_ecto, "~> 3.0-rc"},
-     {:comeonin, "~> 1.5"},
-     {:postgrex, "~> 0.13"},
-     {:excoveralls, "~> 0.4.3", only: [:dev, :test]},
-     {:mock, ">= 0.0.0", only: [:dev, :test]}]
+    [
+      {:phoenix, "~> 1.2.2"},
+      {:comeonin, "~> 3.0"}
+    ]
   end
 end
